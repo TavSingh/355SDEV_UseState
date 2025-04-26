e25b855d38db7c3fe3dbea08112bff1fb2a069e7
TavSingh <tavparsadsingh2@gmail.com>
2025-04-25 23:17:27 -0700

    Completed learning assignment

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,11 +1,17 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
 import { restaurants } from "./data/data.js";
+// Importing useState
+import { useState } from "react";
 import "./App.css";
 
+
 function App() {
+  // Destructuring and saving a shallow copy using the spread operator
+  // useState returns an array with two elements: the first is the current state, and the second is the function used to update that state
+  const [restaurantState, setRestaurants] = useState([...restaurants]);
   return (
     <div className="App">
-      <RestaurantsContainer />
+      <RestaurantsContainer restaurants={restaurantState}/>
     </div>
   );
 }
--- usestate/src/components/Restaurant.jsx
+++ usestate/src/components/Restaurant.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 function Restaurant(props) {
+  // Setting the current state and updated state as constants and will change using useState
+  const [hours, setHours] = useState(false);
+  const [menu, setMenu] = useState(false);
+
+  // Function that will alternate from the opposite of the previous state;
+  // true -> false
+  // false -> true
+  const handleHours = () => {
+    setHours((prev) => !prev);
+  }
+  // Same thing for the menu
+  const handleMenu = () => {
+    setMenu((prev) => !prev);
+  };
   return (
     <div className="restaurant">
       <img src={props.restaurant.image} alt={props.name} />
@@ -9,8 +25,28 @@ function Restaurant(props) {
         <li>{props.restaurant.cuisine}</li>
         <li>{props.restaurant.rating}</li>
       </ul>
-      <button>Hours</button>
-      <button>Menu</button>
+      {/*Using onClick event to change the states*/}
+      <button onClick={handleHours}>Hours</button>
+      <button onClick={handleMenu}>Menu</button>
+      {/* Conditionally rendering information based on the useState for both the hours and menu */}
+      {/* If either menu or hours are set to true, it will display the respective information */}
+      {menu && (
+        <ul>
+          {props.restaurant.menu.map((menuItem, index) => (
+            <li key={index}>{`${menuItem.item} $${menuItem.price}`}</li>
+          ))}
+        </ul>
+      )}
+      {hours && (
+        <ul>
+          {Object.entries(props.restaurant.hours).map(([day, time]) => (
+            <li key={day}>
+              <strong>{day.charAt(0).toUpperCase() + day.slice(1)}:</strong>{" "}
+              {time}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
